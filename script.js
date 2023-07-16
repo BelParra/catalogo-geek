f0469bf08656f6ecb2cf36306e4ae94c27b2086f
Bel <cibeleparra@rocketmail.com>
2023-07-16 01:46:09 -0300

    corrigido h2,código em inglês novamente, removido scroll horizontal fora da lista

--- script.js
+++ script.js
@@ -1,121 +1,79 @@
-const produtosArray = [
+const productsArray = [
 
   {
-    imagem: "./assets/img/paintings/quadro-c3po.jpg",
-    nome: "Robot",
-    preco: "R$250,00",
-    tipo: "Pinturas",
+    image: "./assets/img/paintings/quadro-c3po.jpg",
+    name: "Robot",
+    price: "R$250,00",
+    type: "Paintings",
   },
   {
-    imagem: "./assets/img/paintings/quadro-controle.jpg",
-    nome: "Controle Playstation",
-    preco: "R$99,00",
-    tipo: "Pinturas"
+    image: "./assets/img/paintings/quadro-controle.jpg",
+    name: "Controle Playstation",
+    price: "R$99,00",
+    type: "Paintings"
   },
   {
-    imagem: "./assets/img/paintings/quadro-mario.jpg",
-    nome: "Mario",
-    preco: "R$275,00",
-    tipo: "Pinturas",
+    image: "./assets/img/paintings/quadro-mario.jpg",
+    name: "Mario",
+    price: "R$275,00",
+    type: "Paintings",
   },
   {
-    imagem: "./assets/img/paintings/quadro-pacman.jpg",
-    nome: "PacMan",
-    preco: "R$160,00",
-    tipo: "Pinturas",
+    image: "./assets/img/paintings/quadro-pacman.jpg",
+    name: "PacMan",
+    price: "R$160,00",
+    type: "Paintings",
   },
   {
-    imagem: "./assets/img/paintings/quadro-relogio.jpg",
-    nome: "Smart Watch",
-    preco: "R$120,00",
-    tipo: "Pinturas",
+    image: "./assets/img/paintings/quadro-relogio.jpg",
+    name: "Smart Watch",
+    price: "R$120,00",
+    type: "Paintings",
   },
   {
-    imagem: "./assets/img/actions/dragonball-action-figure.jpg",
-    nome: "Goku Super Saiyajin",
-    preco: "R$500,00",
-    tipo: "Figuras de Ação",
+    image: "./assets/img/actions/dragonball-action-figure.jpg",
+    name: "Goku Super Saiyajin",
+    price: "R$500,00",
+    type: "Action Figures",
   },
   {
-    imagem: "./assets/img/actions/godzilla-action-figure.jpg",
-    nome: "Godzilla",
-    preco: "R$700,00",
-    tipo: "Figuras de Ação",
+    image: "./assets/img/actions/godzilla-action-figure.jpg",
+    name: "Godzilla",
+    price: "R$700,00",
+    type: "Action Figures",
   },
   {
-    imagem: "./assets/img/actions/groot-action-figure.jpg",
-    nome: "Groot",
-    preco: "R$950,00",
-    tipo: "Figuras de Ação",
+    image: "./assets/img/actions/groot-action-figure.jpg",
+    name: "Groot",
+    price: "R$950,00",
+    type: "Action Figures",
   },
   {
-    imagem: "./assets/img/actions/ironman-action-figure.jpg",
-    nome: "Homem de Ferro",
-    preco: "R$1030,00",
-    tipo: "Figuras de Ação",
+    image: "./assets/img/actions/ironman-action-figure.jpg",
+    name: "Homem de Ferro",
+    price: "R$1030,00",
+    type: "Action Figures",
   },
   {
-    imagem: "./assets/img/actions/starwars-action-figure.jpg",
-    nome: "Sr Yoda",
-    preco: "R$870,00",
-    tipo: "Figuras de Ação",
+    image: "./assets/img/actions/starwars-action-figure.jpg",
+    name: "Sr Yoda",
+    price: "R$870,00",
+    type: "Action Figures",
   },
   {
-    imagem: "./assets/img/actions/wolverine-action-figure.jpg",
-    nome: "Wolverine",
-    preco: "R$510,00",
-    tipo: "Figuras de Ação",
+    image: "./assets/img/actions/wolverine-action-figure.jpg",
+    name: "Wolverine",
+    price: "R$510,00",
+    type: "Action Figures",
   },
-
 ];
-document.addEventListener("DOMContentLoaded", function () { //evitar erros de carregamento
 
-  const pinturasList = document.querySelector(".pinturas-lista");
-  const actionFiguresList = document.querySelector(".figurasAcao-lista");
 
-  
-  const paintingsArray = [];
-  const actionFiguresArray = [];
+document.addEventListener("DOMContentLoaded", function () {
+  const { paintingsArray, actionFiguresArray } = separateItems(productsArray);
 
-  
-  function criarElementoProduto(produto) {
-    const item = document.createElement("li");
-    item.classList.add("card");
-
-    const imagem = document.createElement("img");
-    imagem.classList.add('imagem');
-    imagem.setAttribute("src", produto.imagem);
-    imagem.setAttribute("alt", produto.nome);
-
-    const nome = document.createElement("h3");
-    nome.classList.add('nome')
-    nome.textContent = produto.nome;
-
-    const preco = document.createElement("p");
-    preco.classList.add('preco');
-    preco.textContent = produto.preco;
-
-    item.appendChild(imagem);
-    item.appendChild(nome);
-    item.appendChild(preco);
-
-    return item;
-  }
-
-  function separateItems(produtosArray) {
-    for (let i = 0; i < produtosArray.length; i++) {
-      const produto = produtosArray[i];
-      if (produto.tipo === "Pinturas") {
-        const item = criarElementoProduto(produto);
-        paintingsArray.push(item);
-      } else if (produto.tipo === "Figuras de Ação") {
-        const item = criarElementoProduto(produto);
-        actionFiguresArray.push(item);
-      }
-    }
-  }
-
-  separateItems(produtosArray);
+  const pinturasList = document.querySelector(".paintings-list");
+  const actionFiguresList = document.querySelector(".actionFigures-list");
 
   for (let i = 0; i < paintingsArray.length; i++) {
     pinturasList.appendChild(paintingsArray[i]);
@@ -125,3 +83,48 @@ document.addEventListener("DOMContentLoaded", function () { //evitar erros de ca
     actionFiguresList.appendChild(actionFiguresArray[i]);
   }
 });
+
+function createElementProduct(product) {
+  const item = document.createElement("li");
+  item.classList.add("card");
+
+  const image = document.createElement("img");
+  image.classList.add('image');
+  image.setAttribute("src", product.image);
+  image.setAttribute("alt", product.name);
+
+  const name = document.createElement("h3");
+  name.classList.add('name')
+  name.textContent = product.name;
+
+  const price = document.createElement("p");
+  price.classList.add('price');
+  price.textContent = product.price;
+
+  item.appendChild(image);
+  item.appendChild(name);
+  item.appendChild(price);
+
+  return item;
+}
+
+function separateItems(productsArray) {
+  const paintingsArray = [];
+  const actionFiguresArray = [];
+
+  for (let i = 0; i < productsArray.length; i++) {
+    const product = productsArray[i];
+    if (product.type === "Paintings") {
+      const item = createElementProduct(product);
+      paintingsArray.push(item);
+    } else if (product.type === "Action Figures") {
+      const item = createElementProduct(product);
+      actionFiguresArray.push(item);
+    }
+  }
+
+  return {
+    paintingsArray,
+    actionFiguresArray,
+  };
+}
